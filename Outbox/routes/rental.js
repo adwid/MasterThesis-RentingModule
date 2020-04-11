05584a7688cfc413b10294d626c7aa9250da9c23
adwid <adrien.widart@student.uclouvain.be>
2020-04-11 11:45:28 +0200

    [O] fix identation

--- Outbox/routes/rental.js
+++ Outbox/routes/rental.js
@@ -4,26 +4,26 @@ const requestHandler = require('../handler/requestHandler');
 const axios = require('axios');
 
 const routes = {
-  'create':   {inboxRoute:  '/create',  activityGenerator:  requestHandler.generateCreateRentalActivity},
+    'create':   {inboxRoute:    '/create',  activityGenerator:  requestHandler.generateCreateRentalActivity},
 };
 
 router.post('/:route', function(req, res, next) {
-  if (!routes.hasOwnProperty(req.params.route)) {
-    next();
-    return;
-  }
-  const currentRoute = routes[req.params.route];
-  const activity = currentRoute.activityGenerator(req.body);
-  if (!activity) {
-    res.status(400).end();
-    return;
-  }
-  axios.post('http://10.42.0.1:' + process.env.RENTAL_INBOX_PORT + '/rental/secretary' + currentRoute.inboxRoute, activity)
-      .then(_ => res.status(201).end())
-      .catch(err => {
-        console.error("Error(s) while forwarding to secretary : " + err);
-        res.status(500).json({error: "An internal occurred. Please try later or contact admins."})
-      });
+    if (!routes.hasOwnProperty(req.params.route)) {
+        next();
+        return;
+    }
+    const currentRoute = routes[req.params.route];
+    const activity = currentRoute.activityGenerator(req.body);
+    if (!activity) {
+        res.status(400).end();
+        return;
+    }
+    axios.post('http://10.42.0.1:' + process.env.RENTAL_INBOX_PORT + '/rental/secretary' + currentRoute.inboxRoute, activity)
+        .then(_ => res.status(201).end())
+        .catch(err => {
+            console.error("Error(s) while forwarding to secretary : " + err);
+            res.status(500).json({error: "An internal occurred. Please try later or contact admins."})
+        });
 });
 
 module.exports = router;
